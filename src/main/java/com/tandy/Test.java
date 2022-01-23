f1955c9e7d7c2078d9c0ba7112e53f6011c64d88
tandysun <tandysun@tencent.com>
2022-01-23 17:31:45 +0800

    just test, ignore

--- src/main/java/com/tandy/Test.java
+++ src/main/java/com/tandy/Test.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("StringUtils.capitalize(\"abc\") = " + StringUtils.capitalize("abc"));
+        System.out.println("StringUtils.capitalize(\"abcd\") = " + StringUtils.capitalize("abc"));
     }
 }
